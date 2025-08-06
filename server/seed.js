81b38cf6b4f195410b98ef8b3044245132728ee9
soham <soaham1006@gmail.com>
2025-08-06 19:57:25 +0530

    update url

--- server/seed.js
+++ server/seed.js
@@ -8,15 +8,16 @@ mongoose.connect(process.env.MONGO_URI).then(async () => {
   await Project.deleteMany(); 
   await Project.insertMany([
     {
+    title: "Private ChatRoom Application",
+    description: "Developed a real-time, end-to-end encrypted chat app with private room support.Enabled live messaging, typing status, and timestamps using Socket.io.Stored chats securely in MongoDB with user authentication and AES encryption.",
+    link: "https://github.com/soham1006/Sanwariya"
+  },
+    {
       title: "Private Chat-Room",
       description: "A real-time encrypted chat app using Socket.io and MongoDB.",
       link: "https://github.com/soham1006/private-chat-room"
     },
-     {
-    title: "Expense-Tracker",
-    description: "A simple web-based tool to track your expenses visually using HTML, CSS, and JavaScript.",
-    link: "https://github.com/soham1006/Expense-Tracker"
-  },
+     
   {
     title: "wordWizard",
     description: "A JavaScript-based word game app that lets users challenge vocabulary and spelling.",
--- server/server.js
+++ server/server.js
@@ -36,3 +36,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
